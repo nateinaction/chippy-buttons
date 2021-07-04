b3cc1554a1f2a426d8fccb391030475fd5e92bc3
Nate Gay <email@nategay.me>
2021-07-04 00:14:33 -0500

    Simplify sysctl service

--- install.sh
+++ install.sh
@@ -5,5 +5,3 @@ service="chippy-buttons.service"
 sudo cp "$path/$service" /lib/systemd/system/
 sudo chmod 644 "/lib/systemd/system/$service"
 sudo systemctl daemon-reload
-sudo systemctl enable "$service"
-sudo systemctl start "$service"
